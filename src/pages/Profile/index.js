183005bc9ec6b12a4c3d5b031bae84257b938283
Alexandre Lima <alexandreatlima@gmail.com>
2022-12-12 10:53:35 -0300

    logout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { api } from "../../api/api";
+import { AuthContext } from "../../context/authContext";
 
 export function Profile() {
   const navigate = useNavigate();
   const [userData, setUserData] = useState({});
+
+  const { setLoggedInUser } = useContext(AuthContext);
 
   useEffect(() => {
     async function fetchUser() {
@@ -24,12 +27,20 @@ export function Profile() {
     fetchUser();
   }, []);
 
+  function handleLogOut() {
+    localStorage.removeItem("loggedInUser");
+    setLoggedInUser(null);
+    navigate("/");
+  }
+
   return (
     <>
       <h1>{userData.name}</h1>
       <Link to="/produtos">
         <button>Produtos</button>
       </Link>
+
+      <button onClick={handleLogOut}>Sair</button>
     </>
   );
 }
